10cb5c695819b2b2f989ed17be3540f530d10f5a
taneermarne20 <>
2024-01-24 19:46:22 +0530

    questions added

--- Employee_querry.sql
+++ Employee_querry.sql
@@ -1,2 +1,42 @@
-SELECT *
-FROM EMPLOYEES;
+--1) Write a query to display all the records of the employee whose
+--joining_date is after 20 March 2022.
+
+
+--2) Write a query to display emp_no, first_name, last_name, department of
+--the employee whose department is ‘Finance’.
+
+
+
+
+--3) Write a query to display emp_no, salary, department of the employee
+--whose salary is below 35000.
+
+
+
+--4) Write a query to display all the records of the employee whose birth_date
+--is after or equal to 24 January 1998.
+
+
+--5) Write a query to display emp_no, first_name, department of
+--the employee whose department is other than ‘IT’.
+
+
+
+
+
+--6) Write a query to display all the records of the employee whose
+--emp_no is 1010 or below 1010.
+
+
+
+
+
+--7) Write a query to display unique first_name of the employee whose
+--gender is Male.
+
+
+
+
+--8) Write a query to display emp_no, first_name , salary and add 10000
+--to the salary of the employee whose salary is below 25000 and
+--display final salary as column name ‘final_salary’.
