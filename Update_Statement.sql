fdf150a2cf7c78756c04a46c16a298aaa8e19a7c
tanveermarne20 <>
2024-01-30 12:04:28 +0530

    Questions added

--- Update_Statement.sql
+++ Update_Statement.sql
@@ -30,29 +30,37 @@ VALUES ('RYL02', 'Queen doll', 9.49, 26);
 
 SELECT *
 FROM PRODUCTS;
---1) Increase the price of all products by 10%.
 
+--1) Increase the price of all products by 10%.
+UPDATE PRODUCTS 
+SET PRICE = (PRICE*0.10+PRICE);
 
 --2) Set the price of all products with a quantity less than 10 to 5.99.
-
+UPDATE PRODUCTS 
+SET PRICE =5.99 
+WHERE QUANTITY<10;
 
 --3) Double the price of products with a quantity between 15 and 50.
-
+UPDATE PRODUCTS
+SET PRICE=2*PRICE
+WHERE QUANTITY BETWEEN 15 AND 50;
 
 
 --4) Update the product name for a specific product with product_id
 --'RGAN01’ to a new name, “Barbie Doll” and also update price to 6.99.
 
-
-
+UPDATE PRODUCTS
+SET
+PRODUCT_NAME='Barbiee Doll',
+PRICE=6.99
+WHERE PRODUCT_ID='RGAN01';
 
 --5) Increase the price by 5% for products with a name containing the
 --word “toy”
---Questions
 
-
-
-
+UPDATE PRODUCTS
+SET PRICE=PRICE+PRICE*0.05
+WHERE PRODUCT_NAME LIKE '%toy%';
 
 --6) Add a prefix "SALE: " to the beginning of the product name for all
 --products.
